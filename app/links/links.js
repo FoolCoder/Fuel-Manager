557d7d1fd70d21dcce42c42f3110ead6369b349e
Muhammad Qazafi Sheikh <qazafi.sheikh121@gmail.com>
2023-07-16 12:39:49 +0500

    Update links.js

--- app/links/links.js
+++ app/links/links.js
@@ -1,13 +1,1 @@
-// export const link = 'http://18.219.46.195:5000'
-export const link = 'http://a5b5-101-50-108-27.ngrok.io'
 
-// export const filterdadds = '/ad/getFilteredAds'
-// export const useradds = '/ad/getAdsByUser?userId='
-// export const postuseradd = '/ad/insertAd'
-// export const useraddstatues = '/ad/changeSoldStatus?adID='
-
-// export const rentfilterdadds = '/rentOut/getFilteredRentOut'
-// export const rentuseradds = '/rent/getRentByUser?userId='
-// export const rentpostuseradd = '/rentOut/insertRentOut'
-
-// export const apikey = 'AIzaSyA-_ESKOszOV6bSzAqb6frFWMZqnLeQj1U'
